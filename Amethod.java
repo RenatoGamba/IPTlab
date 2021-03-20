35a41e1bbfe8453bf215929ac7f406b1b577e290
AdrianBautistaa <adrianteofilo7@gmail.com>
2021-03-20 16:06:08 +0800

    other methods

---
+++ Amethod.java
@@ -0,0 +1,25 @@
+
+package iipt;
+
+import java.util.Scanner;
+public class Amethod {
+    
+
+Scanner sc = new Scanner(System.in);
+ 
+ public void display1() {
+    System.out.print("Input your name (LN, FN, MI): ");
+    String name = sc.next();
+  }
+
+  public void display2() {
+    System.out.print("Input your age: ");
+     String age = sc.next();
+  }
+  
+   public void display3() {
+    System.out.print("Input your Gender: ");
+     String g = sc.next();
+  }
+   
+}
