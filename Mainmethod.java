17354a9f0ddce0e83805ee05b09b3ebec46a5ce5
RenatoGamba <r.gamba0332@student.tsu.edu.ph>
2021-03-20 15:31:07 +0800

    javacode
    this has a method in it

---
+++ Mainmethod.java
@@ -0,0 +1,18 @@
+package iipt;
+
+public class IIPT {
+
+
+    public static void main(String[] args) {
+            Amethod  obj = new Amethod ();
+    obj.display1();
+    obj.display2();
+    obj.display3();
+    
+            Rmethod obj2 = new Rmethod ();
+    obj2.display4();
+    obj2.display5();
+    obj2.display6();
+    }
+    
+}
---
+++ Rmethod.java
@@ -0,0 +1,18 @@
+package iipt;
+import java.util.Scanner;
+public class Rmethod {
+    Scanner sc = new Scanner(System.in);
+     public void display4() {
+    System.out.print("Input your Religion: ");
+     String rel  = sc.next();
+  }
+
+      public void display5() {
+    System.out.print("Input your address : ");
+     String add = sc.next();
+  }
+         public void display6() {
+    System.out.print("Input your contact number: ");
+     String contact = sc.next();
+  }
+}
